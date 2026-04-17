33602ff211ce1b5fb35810227bb7d620ef9d9f07
Jonathan <davjonathan6@gmail.com>
2026-04-17 14:33:40 +0000

    modif pour env

--- database/connection.py
+++ database/connection.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from database.base import Base
 
 # DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASE_URL ="postgresql+psycopg2://postgres:NouveauMotDePasse@localhost:5432/teambuilding"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
